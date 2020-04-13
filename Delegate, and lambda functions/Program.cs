a0961bdbdcc11482d22f471d7dbe79981f2a1dcc
= <bsef17m051@pucit.edu.pk>
2020-04-13 15:31:02 +0500

    Simple delegate testing done

--- Delegate, and lambda functions/Program.cs
+++ Delegate, and lambda functions/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace Delegate__and_lambda_functions
 {
-    class Program
+    public delegate void ShowDelegate();
+
+    internal class Person
     {
-        static void Main(string[] args)
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Show()
         {
+            if (Name != null) Console.Write($"My name is: {Name}");
+        }
+    }
+
+    public class Program
+    {
+        private Person Person1 { get; set; }
+
+        private static void Main()
+        {
+            var program1 = new Program(){Person1 = new Person(){Id = 1,Name = "Ali"}};
+            ShowDelegate callShow = program1.Person1.Show;
+            callShow();
+            Console.ReadKey();
         }
     }
 }
