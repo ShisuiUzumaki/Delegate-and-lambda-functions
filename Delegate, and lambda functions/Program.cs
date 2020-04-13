7ee131f24cf354ebccd6f2067294087c4c19095f
= <bsef17m051@pucit.edu.pk>
2020-04-13 15:58:19 +0500

    Buit in delegates done

--- Delegate, and lambda functions/Program.cs
+++ Delegate, and lambda functions/Program.cs
@@ -17,6 +17,10 @@ namespace Delegate__and_lambda_functions
         {
             if (Name != null) Console.Write($"My name is: {Name}");
         }
+
+        public int GetUserId() => Id;
+
+        public bool IsValidUser() => Id >= 0;
     }
 
     public class Program
@@ -28,7 +32,44 @@ namespace Delegate__and_lambda_functions
             var program1 = new Program(){Person1 = new Person(){Id = 1,Name = "Ali"}};
             ShowDelegate callShow = program1.Person1.Show;
             callShow();
+
+            //Checking built in delegates
+
+            //Action delegate
+            Action action1 = program1.Person1.Show;
+            action1();
+
+            //Action with arguments
+            Action<int> action2 = program1.Show;
+            action2(12);
+
+            //Func without args
+            Func<int> func1 = program1.Person1.GetUserId;
+            Console.WriteLine(func1());
+
+            //Func with args
+            Func<Person, bool> func2 = program1.CreatePerson;
+            Console.WriteLine(func2(program1.Person1));
+
+            //predicate
+            Predicate<bool> pred = (bool x) => x;
+            Console.WriteLine(pred(true));
+
             Console.ReadKey();
         }
+
+        private bool CreatePerson(Person arg)
+        {
+            
+            return arg.IsValidUser();
+
+        }
+
+        public void Show(int x)
+        {
+            Console.WriteLine(x);
+        }
     }
+
+
 }
